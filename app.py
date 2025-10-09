c0f6f08fab1ed25fd896899ef49090a87ebb05f2
SURYAKUMAR S <96831319+SURYAKUMARS11@users.noreply.github.com>
2025-10-09 14:29:51 +0530

    Update FRONTEND_SIGNUP_BASE_URL to new URL

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ except Exception as e:
     app_logger.error(f"Error initializing Razorpay client: {e}")
     razorpay_client = None
 
-FRONTEND_SIGNUP_BASE_URL = os.environ.get("FRONTEND_SIGNUP_BASE_URL", "https://volt-earning.vercel.app/signup")
+FRONTEND_SIGNUP_BASE_URL = os.environ.get("FRONTEND_SIGNUP_BASE_URL", "https://tesla-invest.vercel.app/")
 
 # --- DECORATOR for Admin Authentication ---
 def admin_required(f):
